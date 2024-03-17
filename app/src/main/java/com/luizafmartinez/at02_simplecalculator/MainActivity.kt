d159aa607b28eae69504fe8c2a65a23b5ebe8bc2
alxmart <alxmartinez@gmail.com>
2024-03-17 19:43:28 -0300

    Fiinshed layout, adding ViewBinding to the project, add logic.

--- app/src/main/java/com/luizafmartinez/at02_simplecalculator/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/at02_simplecalculator/MainActivity.kt
@@ -5,16 +5,43 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizafmartinez.at02_simplecalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+       //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val n1 = binding.edtnumber1.text
+        val n2 = binding.edtNumber2.text
+
+        binding.btnPlus.setOnClickListener {
+            var sumResult = n1.toString().toDouble() + n2.toString().toDouble()
+            binding.txtResult.text = sumResult.toString()
         }
+
+        binding.btnMinus.setOnClickListener {
+            var sumResult = n1.toString().toDouble() - n2.toString().toDouble()
+            binding.txtResult.text = sumResult.toString()
+        }
+
+        binding.btnMultiply.setOnClickListener {
+            var sumResult = n1.toString().toDouble() * n2.toString().toDouble()
+            binding.txtResult.text = sumResult.toString()
+        }
+
+        binding.btnDivide.setOnClickListener {
+            var sumResult = n1.toString().toDouble() / n2.toString().toDouble()
+            binding.txtResult.text = sumResult.toString()
+        }
+
+
     }
 }
